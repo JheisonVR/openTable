0364a0e271ad157b0d8905dead5fc99dab9f0dc3
Jheyson vargas <daviid.vargasr@gmail.com>
2023-07-31 22:38:09 -0500

    First Fetch

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,31 @@
-
 import { Header, RestaurantCard } from './components'
+import { PrismaClient } from '@prisma/client'
+import async from '../pages/api/seed';
 
-export default function Home() {
+const prisma = new PrismaClient();
+
+const fetchRestaurants = async () => {
+  const restaurants = prisma.restaurant.findMany();
+  return restaurants
+}
+
+
+export default async function Home() {
+
+  const restaurants = await fetchRestaurants();
+  // console.log(restaurants)
+
   return (      
         <>          
           <Header/>
           {/* CARDS */}
           <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
+            {restaurants.map((r,i) => (
+              
+              <RestaurantCard
+                key={i}
+              />  
+            ))}
             <RestaurantCard/>
           </div>
         </>
